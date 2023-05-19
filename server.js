277bf72a48ead85f42e11387df72c33ac118045c
Jorge Zapien <zapien.meza.jorge@gmail.com>
2023-05-19 14:28:54 -0600

    Page updated

--- server.js
+++ server.js
@@ -8,32 +8,38 @@ const app = express();
 const App = () => {
     const backgroundImage = 'https://images.hdqwalls.com/wallpapers/space-pink-stars-4k-ld.jpg';
 
-    const containerStyle = {
+    const appStyle = {
         textAlign: 'center',
-        marginTop: '200px',
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
         minHeight: '100vh',
+        background: `url(${backgroundImage}) no-repeat center center fixed`,
+        backgroundSize: 'cover',
+      };
+    
+      const contentStyle = {
+        marginTop: '200px',
       };
 
     return React.createElement(
-      'div',
-      { style: containerStyle },
-      React.createElement(
-        'h1',
-        null,
-        'Te amo, Vivi ',
-        React.createElement('span', { role: 'img', 'aria-label': 'heart' }, '❤️')
-      ),
-      React.createElement('iframe', {
-        src: 'https://drive.google.com/file/d/1ftZW93PuuDiqsFoKCkOgcxOi56NL0ea8/preview',
-        width: '480',
-        height: '360',
-        frameBorder: '0',
-        allowfullscreen: true,
-      })
-    );
+        'div',
+        { style: appStyle },
+        React.createElement(
+          'div',
+          { style: contentStyle },
+          React.createElement(
+            'h1',
+            null,
+            'Te amo, Vivi ',
+            React.createElement('span', { role: 'img', 'aria-label': 'heart' }, '❤️')
+          ),
+          React.createElement('iframe', {
+            src: 'https://drive.google.com/file/d/1ftZW93PuuDiqsFoKCkOgcxOi56NL0ea8/preview',
+            width: '480',
+            height: '360',
+            frameBorder: '0',
+            allowFullScreen: true,
+          })
+        )
+      );
   };
 
 app.get('/', (req, res) => {
